8454a6d22391300fbfcb576d0bd799ad1f997490
RafaelFavetta <rafaelfavetta@gmail.com>
2026-02-18 15:47:52 -0300

    refactor: rename Pacient class to Patient and add arrivalOrder field with setter

--- src/main/java/com/rafaelfavetta/queuesystem/domain/Patient.java
+++ src/main/java/com/rafaelfavetta/queuesystem/domain/Patient.java
@@ -5,15 +5,19 @@ import com.rafaelfavetta.queuesystem.domain.valueObjects.Name;
 import com.rafaelfavetta.queuesystem.domain.valueObjects.Ulid;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 
 @ToString
 @Builder
 @Getter
-public class Pacient {
+public class Patient {
 
     private Ulid id;
     private Age age;
     private PriorityLevel priorityLevel;
     private Name name;
+    @Setter
+    private long arrivalOrder;
+
 }
