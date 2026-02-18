072116f6206e60656b01f2afc75cff631d3be52b
RafaelFavetta <rafaelfavetta@gmail.com>
2026-02-18 15:36:59 -0300

    feat: add Pacient class and its attributes

---
+++ src/main/java/com/rafaelfavetta/queuesystem/domain/Pacient.java
@@ -0,0 +1,11 @@
+package com.rafaelfavetta.queuesystem.domain;
+
+import com.rafaelfavetta.queuesystem.domain.valueObjects.Ulid;
+
+public class Pacient {
+
+    private Ulid id;
+    private Age age;
+    private PriorityLevel priorityLevel;
+    private Name name;
+}
