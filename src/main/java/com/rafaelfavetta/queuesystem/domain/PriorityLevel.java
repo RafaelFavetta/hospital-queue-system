2df82039369237c74f56003e63645883a80215f1
RafaelFavetta <rafaelfavetta@gmail.com>
2026-02-18 15:44:14 -0300

    feat: add PriorityLevel enum with levels and getter

--- src/main/java/com/rafaelfavetta/queuesystem/domain/PriorityLevel.java
+++ src/main/java/com/rafaelfavetta/queuesystem/domain/PriorityLevel.java
@@ -1,4 +1,19 @@
 package com.rafaelfavetta.queuesystem.domain;
 
+import lombok.Getter;
+
+@Getter
 public enum PriorityLevel {
+
+    LOW(1),
+    MEDIUM(2),
+    HIGH(3),
+    EXTREME(4);
+
+    private final int level;
+
+    PriorityLevel(int level) {
+        this.level = level;
+    }
+
 }
