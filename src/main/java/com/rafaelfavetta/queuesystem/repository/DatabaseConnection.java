a53d481a8262b9589dbbca4f3e52934bd977587d
RafaelFavetta <rafaelfavetta@gmail.com>
2026-02-23 00:51:05 -0300

    feat: add DatabaseConnection class for PostgreSQL connection

---
+++ src/main/java/com/rafaelfavetta/queuesystem/repository/DatabaseConnection.java
@@ -0,0 +1,9 @@
+package com.rafaelfavetta.queuesystem.repository;
+
+public class DatabaseConnection {
+
+    private static final String URL = "jdbc:mysql://localhost:5433/queue_system";
+
+    private  static final String USER = "queue";
+    private static final String PASSWORD = "system";
+}
