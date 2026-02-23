45ad4388011655eab0b7423068bc1506529ba308
RafaelFavetta <rafaelfavetta@gmail.com>
2026-02-23 01:06:03 -0300

    feat: update DatabaseConnection for PostgreSQL and add connection method

--- src/main/java/com/rafaelfavetta/queuesystem/repository/DatabaseConnection.java
+++ src/main/java/com/rafaelfavetta/queuesystem/repository/DatabaseConnection.java
@@ -1,9 +1,20 @@
 package com.rafaelfavetta.queuesystem.repository;
 
+import java.sql.Connection;
+
 public class DatabaseConnection {
 
-    private static final String URL = "jdbc:mysql://localhost:5433/queue_system";
+    private static final String URL =
+            "jdbc:postgresql://localhost:5433/queue_system";
 
-    private  static final String USER = "queue";
+    private static final String USER = "queue";
     private static final String PASSWORD = "system";
+
+    public static Connection getConnection() {
+        try {
+            return java.sql.DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (java.sql.SQLException e) {
+            throw new RuntimeException("Error connecting to database", e);
+        }
+    }
 }
